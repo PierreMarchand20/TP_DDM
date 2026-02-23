7df6e622edb910047f9c7aeabf6d1174359b40cc
Alex McSweeney-Davis <alexmcsd@gmail.com>
2026-02-23 13:58:53 +0100

    feat:solve dirichlet on subdomain

--- ddm_project/program2.py
+++ ddm_project/program2.py
@@ -23,6 +23,10 @@ with GmshContextManager(gmsh_options) as mesh_generator:
     
     l1 = gmsh.model.geo.addLine(p1, p2)
     gmsh.model.geo.synchronize()
+
+    boundaries = gmsh.model.getEntities(dim=dim - 1)
+    for i, (_, boundary_tag) in enumerate(boundaries):
+        gmsh.model.addPhysicalGroup(dim - 1, [boundary_tag], name=f"boundary_{i}")
     mesh_global = mesh_generator.generate(dim, nb_partition)
 
 
@@ -59,19 +63,26 @@ As= []
 bs=[]
 Ds=[]
 xs = []
+initial_interface_conditions = []
 
 for i in range(0,nb_partition):
-    A = a.assemble(Vhs[i])
+    A = a.assemble(Vhs[i])    
     b = l.assemble(Vhs[i])
     As.append(A)
     bs.append(b)
     boundary_elem = submeshes[i].physical_group_elements[('interface', 0, None)]
-    D = Vhs[i].get_dofs(boundary_elem)
-    Ds.append(D)
-    x = skfem.solve(*skfem.condense(A, b, D=D))
-    xs.append(x)
+    nodes_on_interface = np.unique(np.concatenate(boundary_elem))
+    initial_interface_conditions.append(np.zeros(A.shape[0]))
+    initial_interface_conditions[i][nodes_on_interface] = 0.5
+    xs.append([])
 
-for i in range(0,nb_partition):
-    boundary_elem = submeshes[i].physical_group_elements[('interface', 0, None)]
-    neigh = neighbors[i]
-    inter = intersections[i]
+
+interface_conditions = initial_interface_conditions
+for iter in range(0,2):
+    # Resolve the local problems    
+    for i in range(0,nb_partition):
+        Linear_system = skfem.enforce(A, b,D=nodes_on_interface,x=interface_conditions[i])
+        x = skfem.solve(*Linear_system)
+        xs[i] = x
+
+    # Exchange information between sub-domains
